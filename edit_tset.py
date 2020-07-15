b74449789edf279a204e1c779ce985b57132adb0
mmshojaefar <34585673+mmshojaefar@users.noreply.github.com>
2020-07-16 02:48:45 +0430

    Update edit_tset.py

--- edit_tset.py
+++ edit_tset.py
@@ -20,7 +20,7 @@ def comment(s):
     
     test_list.append((s_tests,0))
     test_list.append((s,0))                                 #the orginal unittest
-    print(s)
+    # print(s)
     #at last we have: testlist = [(test num n, n), (test num n-1, n-1),...(test num 1, 1), (empty namespace, 0), (full test, 0)]
     return test_list
 
@@ -28,7 +28,7 @@ def grade(s, test_list):                                    #s is the path of gr
     current_path = os.path.abspath(os.getcwd())
     final_grade = []
     os.chdir(s)                                             #first we create test list, then we grade the code
-
+    print(s)
     #open unittest and clear all tests
     with open(r'cpp\aphw1_unittest.cpp', "r+") as f:
         data = f.read()
@@ -38,10 +38,8 @@ def grade(s, test_list):                                    #s is the path of gr
 
     stop = os.popen('docker stop hw1').read()
     ans = os.popen('docker build -t ap1398/hw1 .').read()   #12/12 ...check the code is ok or not
-
-
     if( ans.find('Successfully built') == -1):              #it happens when the code cant compile correctly, so the final grade is zero
-        final_grade = [[i+1,'Compile Error All'] for i in range(len(test_list)-1)]
+        final_grade = [[i+1,'Compile Error All'] for i in range(len(test_list)-2)]
     else:                                                   #now we grade each test
         for i in range(len(test_list)-2):
             #change unittest to grade each test
@@ -50,18 +48,55 @@ def grade(s, test_list):                                    #s is the path of gr
                 f.seek(0)
                 f.write(test_list[i][0])
                 f.truncate()
-
             stop = os.popen('docker stop hw1').read()
-            ans = os.popen('docker build -t ap1398/hw1 .').read()   #12/12 ...
+            ans = os.popen('docker build -t ap1398/hw1 .').read()    #12/12 ...
             if( ans.find('Successfully built') != -1):      #it happens when the code can't compile correctly, so the grade of this test is zero
                 img = os.popen('docker run --rm ap1398/hw1').read() #when the code compile correctlr, we check the result
                 if img.find('<<<SUCCESS>>>') != -1:         #passed
-                    final_grade.append([test_list[i][1],1])
+                    final_grade.append([test_list[i][1],0])
                 else:
-                    final_grade.append([test_list[i][1],0]) #failed
+                    ans = 0
+                    numinus = len(re.findall('minus', img)) #get num of failed test(EXPECT_EQ)
+                    while numinus > 0:
+                        minustr = img[img.rfind("minus"):]
+                        minus = re.findall(r'[-+]?\d*\.\d+|\d+', minustr)[0]    #find the grade of each test(EXPECT_EQ)
+                        ans += float(minus)
+                        img = img.replace(minustr,'')
+                        numinus-=1
+                    final_grade.append([test_list[i][1],ans])           #failed
             else:
                 final_grade.append([test_list[i][1],'Compile Error'])   #copmile error
 
+    with open(r'cpp\aphw1_unittest.cpp', "r+") as f:        #change unittest to first manner
+                data = f.read()
+                f.seek(0)
+                f.write(test_list[-1][0])
+                f.truncate()
     os.chdir(current_path)
+    print(final_grade)
     return final_grade                                      #ex for final grades: [[1,1],[2,0],[3,Compile Error],[4,1]]
                                                             #means test 1 and 4 passed and test 2 failed. test 3 had compile error
+def grade_all(s, test_list):                                #s is hw path
+    grade_list = []
+    os.chdir(s)
+    for student in os.listdir(s):                           #name of folder of courses for each student
+        student = os.path.abspath(student)
+        for sub in os.listdir(student+'\CPlusPlus'):        #sub and answer_sub
+            if sub[:7] == 'Answer_':
+                for ans in os.listdir(student+f'\CPlusPlus\{sub}'):     #each state
+                    stusubs = student+f'\CPlusPlus\{sub}\{ans}'
+                    stu_grade = grade(stusubs,test_list)
+                    grade_list.append([ans,stu_grade])
+    return grade_list
+
+if __name__ == "__main__":
+    os.chdir('.\..')
+    ss = os.getcwd()
+    os.chdir('cplusplus\Answer_MohammadMahdiShojaefar9623065\MohammadMahdiShojaefar9623065_1')
+    s = os.getcwd()
+    ans = comment(s+r'\cpp')
+    s = '\grading\hw'
+    grade_list = grade_all(ss+s,ans)
+    for li in grade_list:
+        for gli in li:
+            print(gli)
