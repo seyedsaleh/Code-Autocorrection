71f0d51a3c14a3a371141f653f45f24a568039ce
mmshojaefar <34585673+mmshojaefar@users.noreply.github.com>
2020-07-29 04:46:24 +0430

    Update student.py

--- student.py
+++ student.py
@@ -1,191 +1,170 @@
 import os
-import glob
-import shutil
-import zipfile
-import itertools
-from pathlib import Path
-
-# def mamadmeti_tree(students_file_address, ta_python, ta_cpp): list of files, func start do this work
-#     '''creats the hw folder'''
-
-def create_tree(s, l, cppfolder, pythonfolder):                         #l is a list consist of name of files
-    ispython = False
-    iscpp = False
-    pythonfilelist = []
-    cppfilelist = []
-    for filename in l:                                                  #determine the language of answers
-        if filename[-3:]=='.py':
-            ispython = True
-            pythonfilelist.append(filename)
-        if filename[-2:]=='.h' or filename[-4:]=='.hpp' or filename[-4:]=='.cpp':
-            iscpp = True
-            cppfilelist.append(filename)
-
-    aphwpath = os.path.abspath(s)                                       #apwh_i path
-    hwpath = Path(os.path.abspath(aphwpath)).parent                     #our folder path(named hw)
-    Path(str(hwpath) + r'\hw').mkdir(parents = True, exist_ok=True)     #create hw in parent of aphw1
-    os.chdir(aphwpath)
-    for student in os.listdir(aphwpath):
-        Path(str(hwpath) + f'\hw\{student}').mkdir(parents = True, exist_ok=True)
-    aphw_pathlist = os.listdir(aphwpath)                                 #list of files in aphw1 folder ()
-    for student in aphw_pathlist:
-        for ans in os.listdir(student):
-            path = os.path.abspath(student)+f'\{ans}' 
-            if(zipfile.is_zipfile(path)):                               #check the file is zip or not(rar or anything else) 
-                                                                        #if there isn't any zip file, the grade will be zero
-                with zipfile.ZipFile(path, 'r') as zip_ref:
-                    Path(str(hwpath) + f'\hw\{student}').mkdir(parents = True, exist_ok=True)
-                    if iscpp:
-                        zip_ref.extractall(str(hwpath) + f'\hw\{student}\CPlusPlus\{ans[:-4]}')     #unzip file in CPlusPlus folder
-                                                                        #ans[:-4] show the name of sub without the '.zip'
-                    if ispython:
-                        zip_ref.extractall(str(hwpath) + f'\hw\{student}\Python\{ans[:-4]}')        #unzip file in Python folder
-                                                                        #ans[:-4] show the name of sub without the '.zip'
-    #now we change dir to 'hw' to create folders to grade easier
-    os.chdir(str(hwpath)+r'\hw')
-    for student in os.listdir(os.path.abspath(os.getcwd())):            #name of students are same as the name of students in aphw_i folder
-        for ans in os.listdir(os.path.abspath(student)):                #CPlusPlus or Python
-            if ans == 'CPlusPlus':
-                create_floder(str(hwpath)+r'\hw'+f'\{student}'+f'\{ans}', 'cpp', cppfilelist, cppfolder)
-            if ans == 'Python':
-                create_floder(str(hwpath)+r'\hw'+f'\{student}'+f'\{ans}', 'python', pythonfilelist, pythonfolder)
-
-
-def create_floder(s, lan, list_name, folder_path):
-    current_dir = os.path.abspath(os.getcwd())                          #the path that we come from to this function
-    os.chdir(s)                                                         #hw\sub\CPlsPlus or Python
-    first_folder = os.listdir(os.getcwd())                              #list of folder in CPlusPlus, actually the subs are in this folder
-    first_dir = os.getcwd()
-    for sub in os.listdir(s):                                           #sub is submission
-        dict = {}                                                       #for ex: dict = {'aphw1.cpp' : [path1, path2, path3]}
-        sub_dir = str(s)+f'\{sub}'
-        for name in list_name:                                          #now search each file(for ex: aphw1.cpp) in sub folder
-            for root, dirs, files in os.walk(sub_dir):
-                for file in files:
-                    if file == name:
-                        path = os.path.join(root, file)
-                        dict.setdefault(name, []).append(path)
-        vals = list(dict.values())
-        vals = check(vals)
-        vals = sorted(vals, key = lambda x:len(x))[::-1]                #sort dict according to the paths list
-        answer = []                                                     #answer is a list of jaygasht ha! :))
-                                                                        #ex for final result: [[aphw1.cpp path1, aphw1.h path1]
-                                                                        #                      [aphw1.cpp path1, aphw1.h path2]]
-        if len(list(dict.keys())) == 1 :
-            answer = list(map(lambda el:[el], list(dict.values())[0]))
-
-        #calculate all state
-        if len(list(dict.keys())) > 1 :
-            for p1,p2 in itertools.product(vals[0],vals[1]):
-                answer.append((p1,p2))
-        temp = answer.copy()
-        for i in range(2,len(vals)):
-            answer.clear()
-            for p1,p2 in itertools.product(temp,vals[i]):
-                answer.append((*p1,p2))
-                temp = answer.copy()
-        
-        #create some folders for grading easier
-        if lan == 'cpp':
-            for i in range(len(answer)):
-                path_folder = first_dir + f'\Answer_{sub}' + f'\{sub}_{i+1}'#folder
-                Path(path_folder).mkdir(parents=True, exist_ok=True)
-                Path(path_folder+r'\cpp').mkdir(parents=True, exist_ok=True)
-                Path(path_folder+r'\h').mkdir(parents=True, exist_ok=True)
-                for file in answer[i]:
-                    for dataset in os.listdir(folder_path+'\dataset'):
-                        shutil.copy(folder_path + '\dataset' + f'\{dataset}', path_folder)
-                    shutil.copy(folder_path + r'\Makefile', path_folder)    #find and copy other files
-                    shutil.copy(folder_path + r'\Dockerfile', path_folder)
-                    shutil.copy(folder_path + r'\aphw_unittest.cpp', path_folder+r'\cpp')
-                    shutil.copy(folder_path + r'\main.cpp', path_folder+r'\cpp')
-                    if(str(file)[-4:]=='.cpp' or str(file)[-4:]=='.hpp'):
-                        shutil.copy(file, path_folder+r'\cpp')
-                    elif str(file)[-2:]=='.h':
-                        shutil.copy(file, path_folder+r'\h')
-        if lan == 'python':
-            for i in range(len(answer)):
-                path_folder = first_dir + f'\Answer_{sub}' + f'\{sub}_{i+1}'#folder
-                Path(path_folder).mkdir(parents=True, exist_ok=True)
-                for file in answer[i]:
-                    for dataset in os.listdir(folder_path+'\dataset'):
-                        shutil.copy(folder_path + '\dataset' + f'\{dataset}', path_folder)
-                    shutil.copy(folder_path + r'\Makefile', path_folder)    #find and copy other files
-                    shutil.copy(folder_path + r'\Dockerfile', path_folder)
-                    shutil.copy(folder_path + r'\aphw_unittest.py', path_folder)
-                    shutil.copy(folder_path + r'\requirements.txt', path_folder)
-                    shutil.copy(file, path_folder)
-    os.chdir(current_dir)           
-    
-
-def check(vals):                                                        #get a list and convert the list to unique-list
-    current_path = os.path.abspath(os.getcwd())
-    output = []
-    #finding same files
-    for li in vals:
-        temp = []
-        tempcode = []
-        licode = []
-        for path in li:
-            with open(path, 'r') as reader:
-                code = reader.read()
-            licode.append(code)
-        for i in range(len(li)):
-            if licode[i] not in tempcode:
-                tempcode.append(licode[i])
-                temp.append(li[i])
-        output.append(temp)
-    os.chdir(current_path)
-    return output
-
-
-def start(s, l, cppfolder, pythonfolder):
-    aphwpath = os.path.abspath(s)
-    hwpath = Path(str(Path(os.path.abspath(aphwpath)).parent) + '\hw')
-    # print(hwpath)
-    if os.path.exists(hwpath):
-        return 'pls remove or rename hw folder'
-    else:
-        create_tree(s, l, cppfolder, pythonfolder)
-        return True
-
-#____________________________________________________________________________
-
-def all_students_grading(root):
-    '''runs run_student() in all the folder in the root'''
-
-#____________________________________________________________________________
+import re
+import numbers
+# import subprocess
+import numpy as np
 
 class student():
-    def __init__(self, setting_Data, folder_Address):
+    def __init__(self, setting_Data, folder_Address, cpp_testlist, python_testlist, cpp_each_test_grade, python_each_test_grade):
         self.setting_data = setting_Data
         self.folder_address = folder_Address
-        self.folder_name = ''
-        self.name = ''
-        self.ID = 0  
+        self.cpp_testlist = cpp_testlist
+        self.python_testlist = python_testlist
+        self.cpp_each_test_grade = cpp_each_test_grade
+        self.python_each_test_grade = python_each_test_grade
+        self.folder_name = os.path.basename(folder_Address)
+        self.farsi_name = ''
+        self.eng_name = ''
+        self.student_id = 0  
 
-        self.grade_list = {}             #key: number of test, value: point loss of that test. (?!)
-        self.build_okay = 0              # 1 if built successfully.
-        self.report_name_format = 0      # 1 if the report name format is okay.
-        self.folder_name_format = 0      # 1 if the folder name format is okay.
-        self.warnings = {}               #key: number of test, value: warning (string)
-        self.images = {}                 #key: number of test, value: image (string)
+        self.grades = []                    #key: number of test, value: point loss of that test. (?!)
+        self.build_okay = 0                 # 1 if built successfully.
+        self.report_name_format = 0         # 1 if the report name format is okay.
+        self.folder_name_format = 0         # 1 if the folder name format is okay.
+        self.warnings = []                  #key: number of test, value: warning (string)
+        self.images = []                    #key: number of test, value: image (string)
+        self.builds = []
         self.python_beauty = 0
         #self.moss = None
         self.final_grade = 0
 
 
-    def pre_process(self):
-        self.folder_name = ''
-        self.name = ''
-        self.ID = 0 
+    def check_format(self, name):                                      #the name must be in shape "MohammadMahdiShojaefar9623065"
+        if len(name) < 7 :
+            return False
+        if ((name[-7:]).isnumeric() and (name[:-7]).isalpha()):
+            return name[:-7], name[-7:]
+        elif ((name[-8:]).isnumeric() and (name[:-8]).isalpha()):
+            return name[:-8], name[-8:]
+        return False
 
-        self.report_name_format = 0      
-        self.folder_name_format = 0
+    def get_name(self):                                           #path of 'xxxxx_name_assi...sub..._file_' in hw
+                                                                   #before this func must check the foldar that contain CPlusPlus or Python
+        pdfname = []
+        subname = []
+        temp = []
+        os.chdir(folder_Address)
+        s = os.path.basename(folder_Address)
+        for ch in s:
+            if not(ch>='a' and ch<='z') and not(ch>='A' and ch<='Z') and not(ch>='0' and ch<='9') and ch != '_':
+                temp.append(ch)
+        subname.append(''.join(map(str, temp)))
+        current_path = os.path.abspath(os.getcwd())
+        for root, dirs, files in os.walk(os.getcwd()):
+            for file in files:
+                if file[-4:] == '.pdf':
+                    pdfname.append(file[:-4])
+        if os.path.exists('CPlusPlus'):
+            os.chdir('CPlusPlus')
+        else:
+            os.chdir('Python')
+        for file in os.listdir(str(os.getcwd())):
+            if file[:7] != 'Answer_':
+                subname.append(file)
+        os.chdir(current_path)
+        return pdfname, subname
+
+    def pre_process(self):
+        pdfname, subname = self.get_name()
+        self.farsi_name = subname[0] 
+        for name in pdfname:
+            if self.check_format(name) != False:
+                self.report_name_format = 1
+                self.eng_name, self.student_id = self.check_format(name)
+        for name in subname:
+            if self.check_format(name) != False:
+                self.folder_name_format = 1
+                self.eng_name, self.student_id = self.check_format(name)
+
+    def findwarning(s):                                         #s is string of warnings
+        warning_list = []
+        s = s.lower()
+        while s.find("warning") != -1:
+            temp1 = s[:s.rfind("warning")]
+            temp = s[temp1.rfind("\n")+1:]
+            warning = temp[:temp.find("\n")]
+            s = s.replace(warning,'')
+            # print(warning)
+            warning_list.append(warning)   
+        return warning_list
+
+    def grade_cppfolder(self, s):                               #s is in shape "CPlusPlus\Answer_name\name_i"
+        current_path = os.path.abspath(os.getcwd())
+        final_grade = []
+        warning_list = []
+        img_list = []
+        build_list = []
+        os.chdir(s)                                             #first we create test list, then we grade the code
+        #open unittest and clear all tests
+        with open(r'cpp\aphw_unittest.cpp', "r+") as f:
+            data = f.read()
+            f.seek(0)
+            f.write(self.cpp_testlist[-2][0])
+            f.truncate()
+        xxx = np.random.randint(1000000)
+        stop = os.popen('docker stop hw1').read()
+        # os.system('docker build --no-cache -t ap1398/hw1 .')
+        ans = os.popen(f'docker build -t ap1398/hw{xxx} --build-arg CACHEBUST=1 .').read()   #12/12 ...check the code is ok or not
+        # print(ans)
+        build_list.append([-1, ans])
+        warning_list = self.findwarning(ans)                    #create warning list
+        if( ans.find('Successfully built') == -1):              #it happens when the code cant compile correctly, so the final grade is zero
+            final_grade = [[i+1,'Compile Error All'] for i in range(len(self.cpp_testlist)-2)]
+        else:                                                   #now we grade each test
+            for i in range(len(self.cpp_testlist)-2):
+                #change unittest to grade each test
+                with open(r'cpp\aphw_unittest.cpp', "r+") as f:
+                    data = f.read()
+                    f.seek(0)
+                    f.write(self.cpp_testlist[i][0])
+                    f.truncate()
+
+                stop = os.popen('docker stop hw1').read()
+                # os.system('docker build -t ap1398/hw1 .')
+                ans = os.popen(f'docker build -t ap1398/hw{xxx} .').read()    #12/12 ...
+                build_list.append([self.cpp_testlist[i][1], ans])
+                if( ans.find('Successfully built') != -1):      #it happens when the code can't compile correctly, so the grade of this test is zero
+                    # warning_list.append([test_list[i][1], findwarning(ans)])
+                    img = os.popen(f'docker run --rm ap1398/hw{xxx}').read() #when the code compile correctlr, we check the result
+                    img_list.append([self.cpp_testlist[i][1], img])
+                    # print(ans)#show warning
+                    # print(img)
+                    if img.find('<<<SUCCESS>>>') != -1:         #passed
+                        final_grade.append([self.cpp_testlist[i][1],0])
+                    else:
+                        ansg = 0
+                        numinus = len(re.findall('minus', img)) #get num of failed test(EXPECT_EQ)
+                        while numinus > 0:
+                            minustr = img[img.rfind("minus"):]
+                            minus = re.findall(r'[-+]?\d*\.\d+|\d+', minustr)[0]    #find the grade of each test(EXPECT_EQ)
+                            ansg += float(minus)
+                            img = img.replace(minustr, '')
+                            numinus -= 1
+                        final_grade.append([self.cpp_testlist[i][1],ansg])          #failed
+                else:
+                    final_grade.append([self.cpp_testlist[i][1],'Compile Error'])   #copmile error
+
+        with open(r'cpp\aphw_unittest.cpp', "r+") as f:        #change unittest to first manner
+                    data = f.read()
+                    f.seek(0)
+                    f.write(self.cpp_testlist[-1][0])
+                    f.truncate()
+        os.chdir(current_path)
+        # print(final_grade)
+        # print(warning_list)
+        return final_grade, warning_list, img_list, build_list  #ex for final grades: [[1,10],[2,0],[3,Compile Error],[4,10]]
+                                                                #means test 1 and 4 passed and test 2 failed. test 3 had compile error
+
+    def final_cppfolder_grade(self, folder_grades_list):
+        final = 0
+        for i in range(len(self.cpp_each_test_grade)):
+            if isinstance(folder_grades_list[i][1], numbers.Number):
+                final += cpp_each_test_grade[i][1]-folder_grades_list[i][1]
+        return final
 
     def grading(self):
         self.build_okay = 0
-        self.grade_list = {}
+        self.grades = {}
         self.warnings = {}
         self.images = {}
 
@@ -202,8 +181,4 @@ class student():
         self.final_grade()
 
 if __name__ == "__main__":
-    #create folder hw
-    l = ['aphw1.cpp','aphw1.h']
-    folder = r'C:\Users\edr\Desktop\ta'
-    folder2 = r'C:\Users\edr\Desktop\ta1'
-    print(start(r'E:\APHW1', l, folder, folder2))
+    print('ok')
